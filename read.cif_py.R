7ff008d0a71e0146602fa57ba3ac4f9f514f8e21
yakomaxa <47655565+yakomaxa@users.noreply.github.com>
2022-05-25 09:05:27 +0900

    Third commit: added ignore_atom_site to read.cif_py.R

--- read.cif_py.R
+++ read.cif_py.R
@@ -1,10 +1,16 @@
 require(reticulate)
 read.cif_py <- function(cif_path=NULL,
-                                 python_path=""){
+                                 python_path="",
+                                 ignore_atom_site=T
+                                 ){
   use_python(python = python_path)
   pdbecif=import("pdbecif")
   reader=pdbecif$mmcif_io$CifFileReader()
-  cif_out=reader$read(file_path = cif_path,output='cif_dictionary')
+  if (ignore_atom_site){
+    cif_out=reader$read(file_path = cif_path,output='cif_dictionary',ignore="_atom_site")
+  }else{
+    cif_out=reader$read(file_path = cif_path,output='cif_dictionary')
+  }
   class(cif_out)="cif_dict"
   return(cif_out)
 }
