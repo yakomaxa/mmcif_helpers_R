527eb2f8ec3c8aa9770fdd89e629813fb330a8cf
yakomaxa <47655565+yakomaxa@users.noreply.github.com>
2022-07-25 04:44:45 +0900

    added option to remove underbar in the name of first layer of mmcif

--- read.cif_py.R
+++ read.cif_py.R
@@ -1,7 +1,8 @@
 require(reticulate)
+require(stringr)
 read.cif_py <- function(cif_path=NULL,
                                  python_path="",
-                                 ignore_atom_site=T
+                                 ignore_atom_site=T,remove_underbar=T,verbose=F
                                  ){
   use_python(python = python_path)
   pdbecif=import("pdbecif")
@@ -11,6 +12,16 @@ read.cif_py <- function(cif_path=NULL,
   }else{
     cif_out=reader$read(file_path = cif_path,output='cif_dictionary')
   }
+  
+  if(remove_underbar){
+    n=names(cif_out[[1]])
+    nn=str_remove(n,"_")
+    names(cif_out[[1]])=nn
+    if (verbose){
+      cat("read.cif_py: The underbar at the head of first layers' element names are removed\n")
+    }
+  }
+  
   class(cif_out)="cif_dict"
   return(cif_out)
 }
